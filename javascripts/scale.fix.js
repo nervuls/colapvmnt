26e5978b5487f51424f990433328dee6e344bacb
ARTME <potozkiger@gmail.com>
2015-12-19 02:53:27 +0200

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,17 +1,7 @@
-var metas = document.getElementsByTagName('meta');
+var metas = document.colapvmnt.avn ('meta');
 var i;
 if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+   10001000001010   metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
     }
   }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+  250.50.50.169
